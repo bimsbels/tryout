623b9c82662f46ce6821c3e330d1c563ca687428
bimsbels <ukmbimbelstis@gmail.com>
2025-03-09 12:36:48 +0700

    hide modal

--- resources/views/views_user/dashboard.blade.php
+++ resources/views/views_user/dashboard.blade.php
@@ -26,16 +26,16 @@ Dashboard
 
     <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
         <defs>
-            <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+            <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>
         </defs>
         <g class="wave1">
-            <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)">
+            <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)"/>
         </g>
         <g class="wave2">
-            <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)">
+            <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)"/>
         </g>
         <g class="wave3">
-            <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
+            <use xlink:href="#wave-path" x="50" y="9" fill="#fff"/>
         </g>
     </svg>
 
@@ -356,27 +356,28 @@ Dashboard
         </div>
     </section><!-- End Contact Section -->
 
-    <div class="modal fade" id="modal-undur">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <button type="button" class="btn float-end mb-2" data-bs-dismiss="modal" aria-label="Close">
-                        X
-                    </button>
-                    <img src="{{ asset('img/modal-undur.png') }}" width="100%" height="100%" alt="Pengunduran Tonas" srcset="">
-                </div>
-            </div>
+{{--    <div class="modal fade" id="modal-undur">--}}
+{{--        <div class="modal-dialog modal-dialog-centered">--}}
+{{--            <div class="modal-content">--}}
+{{--                <div class="modal-body">--}}
+{{--                    <button type="button" class="btn float-end mb-2" data-bs-dismiss="modal" aria-label="Close">--}}
+{{--                        X--}}
+{{--                    </button>--}}
+{{--                    <img src="{{ asset('img/modal-undur.png') }}" width="100%" height="100%" alt="Pengunduran Tonas" srcset="">--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 
 </main><!-- End #main -->
 @endsection
 
 @push('scripts')
-<script>
-    $(document).ready(function() {
-        $("#modal-undur").modal('show');
-    });
-
-</script>
+{{--<script>--}}
+{{--    $(document).ready(function() {--}}
+{{--        $("#modal-undur").modal('show');--}}
+{{--    });--}}
+{{--</script>--}}
 <script>
     (function() {
         "use strict";
